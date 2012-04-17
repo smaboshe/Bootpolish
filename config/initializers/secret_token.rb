64d8bdf8b1c488fb0d931be06c02fbb9ca33542d
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2012-04-17 10:47:19 +0200

    Initial project import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoepolish::Application.config.secret_token = 'bcc024a90a52bd82c4f5a6a481127c4b3b508c1fb34fead2b6c50a89d98c404567db3b290b3226954f3848fd4fbfc8ebd544e3676eaadcd6d0ffe8e30e23fb79'
